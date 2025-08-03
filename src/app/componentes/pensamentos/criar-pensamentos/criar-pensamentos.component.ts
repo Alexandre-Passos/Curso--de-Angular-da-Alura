15d44909d9137895fcafffb7d732fdcf03e792d6
Alexandre Passos <alexandre15passos@gmail.com>
2025-08-03 09:40:59 -0400

    Inicializando o projeto

---
+++ src/app/componentes/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamentos',
+  templateUrl: './criar-pensamentos.component.html',
+  styleUrls: ['./criar-pensamentos.component.css']
+})
+export class CriarPensamentosComponent implements OnInit {
+
+  pensamentos = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: 'modelo1'
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  criarPensamentos() {
+    alert("Novo pensamento criado!")
+  }
+
+  cancelar() {
+    alert("Pensamento cancelado!")
+  }
+}
